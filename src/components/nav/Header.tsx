6c21425e758dc0e4e94d3e06e66102bb718afb80
Ayush <srivastava.ayush582@gmail.com>
2024-08-30 18:53:25 +0530

    updated:ui

---
+++ src/components/nav/Header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { WalletControlBar } from '../../features/wallet/WalletControlBar';
+import Logo from '../../images/logos/app-logo.svg';
+
+export function Header() {
+  return (
+    <header className="px-2 sm:px-6 lg:px-12 pt-3 pb-2 w-full">
+      <div className="flex items-start justify-between">
+        <Link href="/" className="py-0 flex items-center">
+          <Image src={Logo} width={210} alt="" />
+        </Link>
+        <div className="flex flex-col items-end md:flex-row-reverse md:items-start gap-2">
+          <WalletControlBar />
+        </div>
+      </div>
+    </header>
+  );
+}
